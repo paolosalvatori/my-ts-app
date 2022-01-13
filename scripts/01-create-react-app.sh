6da78e6935b7557348568bcda1923d29a00529c9
Paolo Salvatori <leprino@hotmail.com>
2022-01-13 09:35:38 +0100

    Changed scripts

---
+++ scripts/01-create-react-app.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+# For more information, see: 
+# https://create-react-app.dev/docs/getting-started/
+# https://levelup.gitconnected.com/deploy-a-react-app-to-an-azure-blob-storage-using-github-actions-d56dc4b4724d
+npx create-react-app my-app --template typescript
